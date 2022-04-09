2803e64036f6a83c2c3761c59432e2b895844ab8
Abdessamad el akroudi <lsroudi@gmail.com>
2022-04-09 18:18:51 +0200

    implement pop and push

--- src/first.rs
+++ src/first.rs
@@ -17,4 +17,29 @@ impl List {
     pub fn new() -> Self{
         List{head: Link::Empty}
     }
+
+    pub fn push(&mut self, val: i32){
+
+        let new_node = Box::new(Node {
+            elem: val,
+            next: mem::replace(&mut self.head,Link::Empty)
+        });
+
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32>{
+        let result;
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => {
+                result = None;
+            },
+            Link::More(node) => {
+                result = Some(node.elem);
+                self.head = node.next;
+            }
+        }
+
+        return result;
+    }
 }
